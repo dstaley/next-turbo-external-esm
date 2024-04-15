cb5f412834b93e0d9989e8ee7ad09e42cdf3e789
Dylan Staley <88163+dstaley@users.noreply.github.com>
2024-04-15 13:34:29 -0700

    add reproduction

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ["srcset"],
+  },
+};
 
 export default nextConfig;
